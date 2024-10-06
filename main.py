6862e0d5a3ce06e0db30ff005c2188b99259964e
[Tanishq Homkar] <[tanishqhomkart7@gmail.com]>
2024-10-06 13:16:46 +0530

    Update main.py
    Created Loops for each input method

--- main.py
+++ main.py
@@ -7,34 +7,54 @@ def main():
 
     while True:
         try:
+            file_path = ""
 
-            file_path = input("Enter the file path, or type exit to quit: ")
+            while True:
+                file_path = input("Enter the file path, or type exit to quit: ")
 
-            if file_path.lower() == "exit":
-                print("Exiting the program!")
-                break
+                if file_path.lower() == "exit":
+                    print("Exiting the program!")
+                    return
 
-            data = handler.load_file(file_path)
-            print("Data loaded successfully!")
+                try:
+                    data = handler.load_file(file_path)
+                    print("Data loaded successfully!")
+                    break
+                except ValueError as e:
+                    print(f"Error Loading File: {e}")
+                except Exception as e:
+                    print(f"Unexpected Error: {e}")
 
-            columns = handler.get_columns()
-            print("Columns Available: ", columns)
+            while True:
+                try:
+                    columns = handler.get_columns()
+                    print("Columns Available: ", columns)
+                    
+                    disp_rows = int(input("Enter the number of rows to display: "))
+                    handler.display_rows(disp_rows)
+                    break
+                except ValueError as e:
+                    print(f"Invalid number of Rows: {e}")
             
-            disp_rows = int(input("Enter the number of rows to display: "))
-            handler.display_rows(disp_rows)
+            while True:
+                try:
+                    dep_var = input("Enter the name of the dependent variable: ")
+                    handler.set_dependent_variable(dep_var)
+                    break
+                except ValueError as e:
+                    print(f"Invalid input for Dependent Variable: {e}")
 
-            dep_var = input("Enter the name of the dependent variable: ")
-            handler.set_dependent_variable(dep_var)
-
-            indep_vars = input(
-                "Enter the names of the independent variables (comma-separated): ")
-            #indep_vars = [var.strip(',') for var in indep_vars]
-            handler.set_independent_variable(indep_vars)
+            while True:
+                try:
+                    indep_vars = input(
+                        "Enter the names of the independent variables (comma-separated): ")
+                    #indep_vars = [var.strip(',') for var in indep_vars]
+                    handler.set_independent_variable(indep_vars)
+                    break
+                except ValueError as e:
+                    print(f"Invalid input for Independent Variables: {e}")
 
             handler.disp_sel_var()
-
-        except ValueError as e:
-            print(f"Error: {e}")
 
         except Exception as e:
             print(f"Unexpected Error: {e}")
